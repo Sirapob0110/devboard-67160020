71a71eebca68059b98b6c495ca984645f0f855fe
Sirapob0110 <67160020@go.buu.ac.th>
2026-03-12 14:36:24 +0700

    Add hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("โหลดข้อมูลไม่สำเร็จ");
+
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
